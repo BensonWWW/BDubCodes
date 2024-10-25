d06e286dace87fa667ad574c71c2f6c33919f41a
BT_1N3_14 <sample@bths.edu>
2024-10-25 13:16:36 -0400

    added new variable and print statement

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Worlds! I am Ben!");
         System.out.println("How is everyone today?");
-        System.out.println("I am good!");
+        System.out.println("I am great!");
+
+        int num = 1;
+        System.out.println(num);
     }
 }
